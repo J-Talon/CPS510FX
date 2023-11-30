3aed24b5f7a70fd72ae358ec7b7fe8a4f1efd64e
Minh <qminh30k3@gmail.com>
2023-11-30 13:58:30 -0500

    Add frontend

--- src/main/java/com/example/project510fx/Frontend/AddDelPageController.java
+++ src/main/java/com/example/project510fx/Frontend/AddDelPageController.java
@@ -1,8 +1,44 @@
 package com.example.project510fx.Frontend;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+
+import com.example.project510fx.DatabaseSystem.LibrarySystem;
+import com.example.project510fx.DatabaseSystem.QueryMenu;
+import com.example.project510fx.DatabaseSystem.TableMenu;
+import com.example.project510fx.Entities.Librarian;
+import com.example.project510fx.Entities.Transaction;
+import com.example.project510fx.Util.DatabaseConnection;
+import com.example.project510fx.DatabaseSystem.LibrarySystem;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class AddDelPageController {
+
+    @FXML
+    private TextField memInfo;
+    @FXML
+    private TextField memID;
+    @FXML
+    private TextField medInfo;
+    @FXML
+    private TextField medID;
+    @FXML
+    private void addMem (ActionEvent event){
+        String memberInfo = memInfo.getText();
+
+    }
+    @FXML
+    private void delMem (ActionEvent event) {
+        String memID = memInfo.getText();
+}
+    @FXML
+    private void addMed(ActionEvent event) {
+
+}
+    @FXML
+    private void delMed(ActionEvent event) {
+
+}
 
 }
